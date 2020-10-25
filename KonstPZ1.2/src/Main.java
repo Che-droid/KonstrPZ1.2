b4b1ca48ed498864487699ffb289cd0b9d61af5f
KaterynaChesnokova <katerinach098@gmail.com>
2020-10-25 23:14:20 +0200

    Output changed

--- KonstPZ1.2/src/Main.java
+++ KonstPZ1.2/src/Main.java
@@ -4,11 +4,11 @@ public class Main
         PersonInfo p1=new PersonInfo("Oleg","Popov",33,"London");
         PersonInfo copyP1=p1.Clone();
 
-        System.out.println(p1.toString());
-        System.out.println(copyP1.toString());
+        System.out.println("p1: " + p1.toString());
+        System.out.println("copyP1: " + copyP1.toString());
 
-        System.out.println(System.identityHashCode(p1));
-        System.out.println(System.identityHashCode(copyP1));
+        System.out.println("Hash value out of p1 identity: " + System.identityHashCode(p1));
+        System.out.println("Hash value out of copyP1 identity: " + System.identityHashCode(copyP1));
     }
 
 }
